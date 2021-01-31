5a3b70bc0a5ac2659d3ac1044c9a90fee0005c7f
Indresh <indresharma.01@outlook.com>
2021-01-31 23:02:23 +0530

    updates

--- rsbakery/users/urls.py
+++ rsbakery/users/urls.py
@@ -8,7 +8,7 @@ app_name = 'users'
 
 
 urlpatterns = [
-    path('register/', user_views.RegisterView.as_view(), name='register'),
+    # path('register/', user_views.RegisterView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.ProfileView.as_view(), name='profile'),
